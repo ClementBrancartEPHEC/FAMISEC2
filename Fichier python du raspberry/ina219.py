86d7996f1435ea9dda319897a631e48327b8bee5
ClementBrancartEPHEC <54244365+ClementBrancartEPHEC@users.noreply.github.com>
2019-08-18 20:31:53 +0200

    ina219

---
+++ Fichier python du raspberry/ina219.py
@@ -0,0 +1,40 @@
+import pymysql
+from ina219 import INA219, DeviceRangeError
+from time import sleep
+
+SHUNT_OHMS = 0.1
+MAX_EXPECTED_AMPS = 2.0
+ina = INA219(SHUNT_OHMS, MAX_EXPECTED_AMPS)
+ina.configure(ina.RANGE_16V)
+
+#connexion database
+conn = pymysql.connect(host='127.0.0.1',user='root',password='password',db='famisec')
+#conn = pymysql.connect(host='famisecbtaclem19.mysql.db',user='famisecbtaclem19',password='Brancart18',db='famisecbtaclem19')
+mycursor = conn.cursor()
+
+def read_ina219():
+    try:
+        print('Bus Current: {0:0.2f}mA'.format(ina.current()))
+        if ina.current()>250:
+                sql = "UPDATE etatmachine SET fonction  = 'en cours' WHERE nom='machineUne'"
+                mycursor.execute(sql)
+                conn.commit()
+                print('en cours d utilisation')
+        else:
+                if 100<ina.current():
+                        sql = "UPDATE etatmachine SET fonction  = 'allumee' WHERE nom='machineUne'"
+                        mycursor.execute(sql)
+                        conn.commit()
+                        print('allumee')
+                else:
+                        sql = "UPDATE etatmachine SET fonction = 'eteinte' WHERE nom='machineUne'"
+                        mycursor.execute(sql)
+                        conn.commit()
+                        print('eteinte')
+    except DeviceRangeError as e:
+        # Current out of device range with specified shunt resister
+        print(e)
+
+while 1:
+    read_ina219()
+    sleep(1)
---
+++ Fichier python du raspberry/ky-003.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# coding=utf-8
+
+# Les modules nécessaires sont importés et mis en place
+import RPi.GPIO as GPIO
+import time
+import pymysql
+
+GPIO.setmode(GPIO.BCM)
+
+# La broche d'entrée du capteur est déclarée. En outre la résistance de Pull-up est activée.
+GPIO_PIN = 24
+GPIO.setup(GPIO_PIN, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+print ("Sensor-Test:Ctrl + C pour terminer le test")
+
+#connexion mysql
+conn = pymysql.connect(host='127.0.0.1',user='root',password='password',db='famisec')
+#conn = pymysql.connect(host='famisecbtaclem19.mysql.db',user='famisecbtaclem19',password='Brancart18',database='famisecbtaclem19',unix_socket='TCP')
+mycursor = conn.cursor()
+
+# Cette fonction de sortie est exécutée par détection du signal
+def fonctionDeSortie(self):
+
+        if GPIO.input(24):
+            print("Signal pas détecté")
+            sql = "UPDATE etatmachine SET porte = 'ouverte' WHERE nom='machineUne'"
+            mycursor.execute(sql)
+            conn.commit()
+        else:
+            print("Signal détecté")
+            sql = "UPDATE etatmachine SET porte = 'fermee' WHERE nom='machineUne'"
+            mycursor.execute(sql)
+            conn.commit()
+        time.sleep(5)
+
+# Lors de la détection d'un signal (front descendant du signal) de la fonction de sortie est déclenchée
+GPIO.add_event_detect(GPIO_PIN, GPIO.BOTH, callback=fonctionDeSortie)
+
+
+# Boucle de programme principale
+try:
+    while True:
+        fonctionDeSortie(0)
+
+
+# réinitialisation de tous les GPIO en entrées
+except KeyboardInterrupt:
+        GPIO.cleanup()
+
+
