584aa4b35e382888c64e96fe0dd8ae16b2c8ddf9
ClementBrancartEPHEC <54244365+ClementBrancartEPHEC@users.noreply.github.com>
2019-08-19 20:20:42 +0200

    Update ina219.py

--- Fichier python du raspberry/ina219.py
+++ Fichier python du raspberry/ina219.py
@@ -9,7 +9,7 @@ ina.configure(ina.RANGE_16V)
 
 #connexion database
 #conn = pymysql.connect(host='127.0.0.1',user='root',password='password',db='famisec')
-conn = pymysql.connect(host='famisecbtaclem19.mysql.db',user='famisecbtaclem19',password='Brancart18',db='famisecbtaclem19')
+conn = pymysql.connect(host='xxxxxxxx',user='xxxxxxxxxxx',password='xxxxxxxxxx',db='xxxxxxxxxx')
 mycursor = conn.cursor()
 
 def read_ina219():
